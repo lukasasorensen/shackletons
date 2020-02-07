7f434110d8c1c2893dd92a3020864eb269e80440
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-02-07 17:50:24 -0600

    remove leading slashes from assets

--- precache-manifest.56cbb9b24161481860da016a2ce68967.js
+++ precache-manifest.56cbb9b24161481860da016a2ce68967.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "881119526ec4d38826d0",
-    "url": "/assets/css/main.047dffe0.chunk.css"
+    "url": "assets/css/main.047dffe0.chunk.css"
   },
   {
     "revision": "80352c00768c79b74299",
-    "url": "/assets/js/2.14f4a3cb.chunk.js"
+    "url": "assets/js/2.14f4a3cb.chunk.js"
   },
   {
     "revision": "881119526ec4d38826d0",
-    "url": "/assets/js/main.02478dcb.chunk.js"
+    "url": "assets/js/main.02478dcb.chunk.js"
   },
   {
     "revision": "673d6ad1812774659ed2",
-    "url": "/assets/js/runtime-main.0782417e.js"
+    "url": "assets/js/runtime-main.0782417e.js"
   },
   {
     "revision": "85f06b11051cf84df9f06677d7135771",
-    "url": "/assets/media/shackletons-edited.85f06b11.png"
+    "url": "assets/media/shackletons-edited.85f06b11.png"
   }
 ]);
