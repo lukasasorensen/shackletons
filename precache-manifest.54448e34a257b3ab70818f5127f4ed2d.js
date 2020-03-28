5f13c58b5b180cfc1553151c16d97b296969ef1c
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-03-28 17:07:08 -0500

    updates

--- precache-manifest.54448e34a257b3ab70818f5127f4ed2d.js
+++ precache-manifest.54448e34a257b3ab70818f5127f4ed2d.js
@@ -5,58 +5,58 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "fd9ecfeee8ee0b17f952",
-    "url": "/static/css/3.70d6c955.chunk.css"
+    "url": "assets/css/3.70d6c955.chunk.css"
   },
   {
     "revision": "4d2f5309c156143fdedd",
-    "url": "/static/css/4.9a9c5ec0.chunk.css"
+    "url": "assets/css/4.9a9c5ec0.chunk.css"
   },
   {
     "revision": "d0ab2d9dd0709d6709ed",
-    "url": "/static/css/5.d9493372.chunk.css"
+    "url": "assets/css/5.d9493372.chunk.css"
   },
   {
     "revision": "91eeb61308f2a3b40a55",
-    "url": "/static/css/6.0d7b810a.chunk.css"
+    "url": "assets/css/6.0d7b810a.chunk.css"
   },
   {
     "revision": "d73b7dab04053083901f",
-    "url": "/static/css/main.29d6c1e0.chunk.css"
+    "url": "assets/css/main.29d6c1e0.chunk.css"
   },
   {
     "revision": "fa2be50913fcb64cc5af",
-    "url": "/static/js/2.2215dafa.chunk.js"
+    "url": "assets/js/2.2215dafa.chunk.js"
   },
   {
     "revision": "fd9ecfeee8ee0b17f952",
-    "url": "/static/js/3.2ac3b00e.chunk.js"
+    "url": "assets/js/3.2ac3b00e.chunk.js"
   },
   {
     "revision": "4d2f5309c156143fdedd",
-    "url": "/static/js/4.c9c5b804.chunk.js"
+    "url": "assets/js/4.c9c5b804.chunk.js"
   },
   {
     "revision": "d0ab2d9dd0709d6709ed",
-    "url": "/static/js/5.6a558811.chunk.js"
+    "url": "assets/js/5.6a558811.chunk.js"
   },
   {
     "revision": "91eeb61308f2a3b40a55",
-    "url": "/static/js/6.7e1f9072.chunk.js"
+    "url": "assets/js/6.7e1f9072.chunk.js"
   },
   {
     "revision": "d73b7dab04053083901f",
-    "url": "/static/js/main.b0c2f5e8.chunk.js"
+    "url": "assets/js/main.b0c2f5e8.chunk.js"
   },
   {
     "revision": "fd70252f79a42f0abdbe",
-    "url": "/static/js/runtime-main.4fc757ec.js"
+    "url": "assets/js/runtime-main.4fc757ec.js"
   },
   {
     "revision": "88714a27c1a6c90148b5793a4b8ad8cb",
-    "url": "/static/media/loading.88714a27.gif"
+    "url": "assets/media/loading.88714a27.gif"
   },
   {
     "revision": "85f06b11051cf84df9f06677d7135771",
-    "url": "/static/media/shackletons-edited.85f06b11.png"
+    "url": "assets/media/shackletons-edited.85f06b11.png"
   }
 ]);
